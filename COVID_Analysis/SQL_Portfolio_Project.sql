9c5c376b56c4cd104f73baea1a7815d591d8e779
Sanjay Jyakhwa <jyakhwa.sanjay0505@gmail.com>
2023-07-09 23:26:57 +0100

    New file

---
+++ COVID_Analysis/SQL_Portfolio_Project.sql
@@ -0,0 +1,20 @@
+------SQL Data Exploration-----------------SANJAY JYAKHWA---------------
+
+USE SQL_Portfolio_Project;
+
+--SELECT * 
+--FROM SQL_Portfolio_Project..CovidDeaths;
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
